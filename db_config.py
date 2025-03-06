a91203533c20aea8b8dcc5c34b2bb84029ee0552
Michael <56340359+SynysterRev@users.noreply.github.com>
2025-03-06 09:03:55 +0100

    create base project

---
+++ db_config.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOSTNAME = os.getenv("DB_HOSTNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+db_url = (f"postgresql+psycopg2://{DB_HOSTNAME}:{DB_PASSWORD}@localhost:{DB_PORT}"
+          f"/{DB_NAME}")
+
+if db_url is None:
+    raise ValueError("Environment variables must be set")
+
+engine = create_engine(db_url)
---
+++ main.py
@@ -0,0 +1,24 @@
+from sqlalchemy.orm import Session
+
+from db_config import engine
+
+
+def main():
+
+    try:
+        with engine.connect() as connection:
+            print("Connection established")
+    except Exception as ex:
+        print(ex)
+
+    with Session(engine) as session:
+        try:
+            session.add('SELECT 1')
+            print("ca marche")
+        except Exception as ex:
+            print(ex)
+        finally:
+            session.close()
+
+if __name__ == '__main__':
+    main()
