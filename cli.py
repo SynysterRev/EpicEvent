d12768a44cefc58508d993040355e52de732bbff
Michael <56340359+SynysterRev@users.noreply.github.com>
2025-04-11 13:45:34 +0200

    add sentry dsn in .env file

--- cli.py
+++ cli.py
@@ -1,12 +1,14 @@
+import os
+
 import click
 
 import sentry_sdk
+from dotenv import load_dotenv
 
+load_dotenv()
 
 sentry_sdk.init(
-    dsn="https://5b3f5cdfc81bac57dcca89423b862b67@o4509082863665152.ingest.de.sentry.io/4509082874675280",
-    # Add data like request headers and IP for users,
-    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    dsn=os.getenv("SENTRY_DSN"),
     send_default_pii=True,
 )
 
